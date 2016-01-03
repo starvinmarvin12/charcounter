5ba2156fa9740ec821e1308b9d7c2e0f61fb4a94
Mike Cohen <mmikecohen@gmail.com>
2016-01-03 08:02:44 -0800

    Counts entered characters correctly, does not echo back using character array

--- Test122715.cc
+++ Test122715.cc
@@ -2,18 +2,23 @@
 
 int main() {
 
-	int count ;
-	
-	for (count = 1; count <+ 10; count++) {
+	char c = 0 ;			// Variable to hold character input by user
+	//char sentence[80] ; 	// Create a character array
 
-		if (count == 5) {
-        	continue ;
-        }
+	int count = 0 ;			// Input character count initialized
 
-        printf("%d",count) ;
+	// Prompt user to enter a line of text
+	puts("Enter a line of text") ;
+
+	// Read in and count each entered character
+	while((c = getchar()) != '\n') {
+
+		count++ ;
+	//	sentence[count++] = c ;
 	}
 
-	printf("\nUsed continue to skip printing the value 5 \n" ) ;
-
+	printf(" The number of characters entered is %d", count) ;
+	puts("\n The characters entered are:") ;
+	
 	return 0 ;
 } 
